4f57cb0e81876ced9495ac87cf78255ad8648241
Dhruv Suthar <git@dhrv.pw>
2026-06-17 22:55:40 +0530

    fix(extensions): normalize axios headers and extend HTML entity decoding - Add `toPlainHeaders` helper to convert AxiosHeaders to plain objects. - Update Result view to render and copy normalized headers with stringified values. - Enhance `trimTagsAndDecodeEntities` to decode hex/decimal numeric references, &nbsp;, em dash, en dash, ellipsis, copyright, and reorder ampersand handling.

--- extensions/lark/src/utils/string.ts
+++ extensions/lark/src/utils/string.ts
@@ -1,10 +1,17 @@
 export function trimTagsAndDecodeEntities(str: string): string {
   const withoutTags = str.replace(/<[^>]*>/g, "");
   return withoutTags
-    .replace(/&amp;/g, "&")
+    .replace(/&#x([0-9a-fA-F]+);/g, (_, hex) => String.fromCodePoint(parseInt(hex, 16)))
+    .replace(/&#([0-9]+);/g, (_, dec) => String.fromCodePoint(parseInt(dec, 10)))
+    .replace(/&nbsp;/g, "\u00a0")
     .replace(/&lt;/g, "<")
     .replace(/&gt;/g, ">")
     .replace(/&quot;/g, '"')
     .replace(/&#39;/g, "'")
-    .replace(/&apos;/g, "'");
+    .replace(/&apos;/g, "'")
+    .replace(/&mdash;/g, "\u2014")
+    .replace(/&ndash;/g, "\u2013")
+    .replace(/&hellip;/g, "\u2026")
+    .replace(/&copy;/g, "\u00a9")
+    .replace(/&amp;/g, "&");
 }
