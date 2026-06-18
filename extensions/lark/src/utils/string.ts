21efbdcfab799dd7c72bbc65f5a4a05c41dff561
Per Nielsen Tikær <per@raycast.com>
2026-06-18 07:28:16 +0200

    Update

--- extensions/lark/src/utils/string.ts
+++ extensions/lark/src/utils/string.ts
@@ -1,8 +1,20 @@
 export function trimTagsAndDecodeEntities(str: string): string {
   const withoutTags = str.replace(/<[^>]*>/g, "");
   return withoutTags
-    .replace(/&#x([0-9a-fA-F]+);/g, (_, hex) => String.fromCodePoint(parseInt(hex, 16)))
-    .replace(/&#([0-9]+);/g, (_, dec) => String.fromCodePoint(parseInt(dec, 10)))
+    .replace(/&#x([0-9a-fA-F]+);/g, (match, hex) => {
+      try {
+        return String.fromCodePoint(parseInt(hex, 16));
+      } catch {
+        return match;
+      }
+    })
+    .replace(/&#([0-9]+);/g, (match, dec) => {
+      try {
+        return String.fromCodePoint(parseInt(dec, 10));
+      } catch {
+        return match;
+      }
+    })
     .replace(/&nbsp;/g, "\u00a0")
     .replace(/&lt;/g, "<")
     .replace(/&gt;/g, ">")
